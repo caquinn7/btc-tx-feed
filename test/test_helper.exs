99d6dff11118dcc163a7edf7b1d8deebda192ff7
Christopher Quinn <caquinn7@gmail.com>
2026-03-15 10:33:55 -0500

    delete test db files after test run

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,11 @@ ExUnit.start()
 
 Ecto.Migrator.run(BtcTxFeed.Repo, :up, all: true)
 Ecto.Adapters.SQL.Sandbox.mode(BtcTxFeed.Repo, :manual)
+
+ExUnit.after_suite(fn _ ->
+  db_path =
+    Application.fetch_env!(:btc_tx_feed, BtcTxFeed.Repo)
+    |> Keyword.fetch!(:database)
+
+  Enum.each([db_path, db_path <> "-shm", db_path <> "-wal"], &File.rm/1)
+end)
