8bd6ad91eda9377ab25fd9017754c4caaa9d93e9
Christopher Quinn <caquinn7@gmail.com>
2026-03-16 22:21:22 -0500

    address deployment issues
    - increase ram
    - add health check endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :btc_tx_feed, BtcTxFeedWeb.Endpoint,
   force_ssl: [
     rewrite_on: [:x_forwarded_proto],
     exclude: [
-      # paths: ["/health"],
+      paths: ["/health"],
       hosts: ["localhost", "127.0.0.1"]
     ]
   ]
