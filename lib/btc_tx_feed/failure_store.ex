9525a851a5f4ed68b77ac91bac7675b081d2effe
Christopher Quinn <caquinn7@gmail.com>
2026-03-21 17:49:14 -0500

    Steps 5 & 6
    Update `FailureStore`
    Update `TxSampler`

--- lib/btc_tx_feed/failure_store.ex
+++ lib/btc_tx_feed/failure_store.ex
@@ -3,16 +3,26 @@ defmodule BtcTxFeed.FailureStore do
 
   alias BtcTxFeed.{Failure, Repo}
 
-  def insert(txid, raw_bytes, reason) do
+  def insert(txid, raw_bytes, reason, session_id) do
     raw_hex = Base.encode16(raw_bytes, case: :lower)
 
     Repo.insert!(%Failure{
       txid: txid,
       raw_hex: raw_hex,
-      error_reason: inspect(reason)
+      error_reason: inspect(reason),
+      stats_session_id: session_id
     })
 
     :ok
+  end
+
+  def list_for_session(session_id) do
+    Repo.all(
+      from(f in Failure,
+        where: f.stats_session_id == ^session_id,
+        order_by: [desc: f.inserted_at]
+      )
+    )
   end
 
   def list_recent(limit \\ 50) do
