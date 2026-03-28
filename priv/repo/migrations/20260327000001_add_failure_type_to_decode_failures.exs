12707f1c85b6db54578cd31f3309ef87b7806a78
Christopher Quinn <caquinn7@gmail.com>
2026-03-28 11:56:56 -0500

    adjust indexes on `decode_failures`

--- priv/repo/migrations/20260327000001_add_failure_type_to_decode_failures.exs
+++ priv/repo/migrations/20260327000001_add_failure_type_to_decode_failures.exs
@@ -5,5 +5,9 @@ defmodule BtcTxFeed.Repo.Migrations.AddFailureTypeToDecodeFailures do
     alter table(:decode_failures) do
       add :failure_type, :string, null: false, default: "decode"
     end
+
+    drop index(:decode_failures, [:inserted_at])
+    create index(:decode_failures, [:failure_type, :inserted_at])
+    create index(:decode_failures, [:stats_session_id, :failure_type, :inserted_at])
   end
 end
