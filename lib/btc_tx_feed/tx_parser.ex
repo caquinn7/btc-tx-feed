bac334e4db4e135b0bf0362f9444685074f0beae
Christopher Quinn <caquinn7@gmail.com>
2026-04-05 10:53:08 -0500

    enrich `details_map` returned from `TxParser`

--- lib/btc_tx_feed/tx_parser.ex
+++ lib/btc_tx_feed/tx_parser.ex
@@ -23,18 +23,22 @@ defmodule BtcTxFeed.TxParser do
     is_segwit = :btc_tx.is_segwit(tx)
     inputs = :btc_tx.get_inputs(tx)
     outputs = :btc_tx.get_outputs(tx)
+    extracted_outputs = extract_outputs(outputs)
 
-    base = %{
-      version: :btc_tx.get_version(tx),
-      is_segwit: is_segwit,
-      lock_time: :btc_tx.get_lock_time(tx),
-      has_coinbase_marker: :btc_tx.has_coinbase_marker(tx),
-      input_count: length(inputs),
-      output_count: length(outputs),
-      inputs: extract_inputs(inputs),
-      outputs: extract_outputs(outputs),
-      witnesses: extract_witnesses(tx, is_segwit)
-    }
+    base =
+      %{
+        version: :btc_tx.get_version(tx),
+        is_segwit: is_segwit,
+        lock_time: :btc_tx.get_lock_time(tx),
+        has_coinbase_marker: :btc_tx.has_coinbase_marker(tx),
+        input_count: length(inputs),
+        output_count: length(outputs),
+        inputs: extract_inputs(inputs),
+        outputs: extracted_outputs,
+        witnesses: extract_witnesses(tx, is_segwit)
+      }
+      |> Map.merge(build_output_script_summaries(extracted_outputs))
+      |> Map.merge(build_witness_summaries(tx, is_segwit))
 
     case :btc_tx.validate_consensus(tx) do
       {:ok, validated} ->
@@ -60,6 +64,64 @@ defmodule BtcTxFeed.TxParser do
         base
         |> Map.put(:validated, false)
         |> Map.put(:validation_errors, validation_errors)
+    end
+  end
+
+  defp build_output_script_summaries(extracted_outputs) do
+    types = Enum.map(extracted_outputs, & &1.script_type)
+    counts = Enum.frequencies(types)
+
+    %{
+      output_script_types: types,
+      output_script_type_counts: counts,
+      has_op_return: Map.has_key?(counts, :null_data),
+      op_return_output_count: Map.get(counts, :null_data, 0),
+      has_non_standard_output: Map.has_key?(counts, :non_standard)
+    }
+  end
+
+  defp build_witness_summaries(_tx, false) do
+    %{
+      witness_item_counts_per_input: [],
+      witness_total_items: 0,
+      witness_total_bytes: 0,
+      largest_witness_item_bytes: 0,
+      inputs_with_witness_count: 0
+    }
+  end
+
+  defp build_witness_summaries(tx, true) do
+    case :btc_tx.get_witnesses(tx) do
+      {:error, nil} ->
+        %{
+          witness_item_counts_per_input: [],
+          witness_total_items: 0,
+          witness_total_bytes: 0,
+          largest_witness_item_bytes: 0,
+          inputs_with_witness_count: 0
+        }
+
+      {:ok, stacks} ->
+        per_input =
+          Enum.map(stacks, fn stack ->
+            items = :btc_tx.get_witness_items(stack)
+
+            sizes =
+              Enum.map(items, fn item -> byte_size(:btc_tx.get_witness_item_bytes(item)) end)
+
+            {length(items), sizes}
+          end)
+
+        item_counts = Enum.map(per_input, fn {count, _} -> count end)
+        all_sizes = Enum.flat_map(per_input, fn {_, sizes} -> sizes end)
+
+        %{
+          witness_item_counts_per_input: item_counts,
+          witness_total_items: Enum.sum(item_counts),
+          witness_total_bytes: Enum.sum(all_sizes),
+          largest_witness_item_bytes: if(all_sizes == [], do: 0, else: Enum.max(all_sizes)),
+          inputs_with_witness_count: Enum.count(item_counts, fn count -> count > 0 end)
+        }
     end
   end
 
