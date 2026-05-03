deffa013f47737284e6c1759017771fb72de736b
Christopher Quinn <github@cqmail.xyz>
2026-05-03 10:12:03 -0500

    add new fns to `MempoolHttpClient`

--- lib/btc_tx_feed/mempool_http_client.ex
+++ lib/btc_tx_feed/mempool_http_client.ex
@@ -6,4 +6,22 @@ defmodule BtcTxFeed.MempoolHttpClient do
       {:error, ex} -> {:error, ex}
     end
   end
+
+  def get_recent_blocks do
+    case Req.get("https://mempool.space/api/blocks") do
+      {:ok, %{status: 200, body: body}} -> {:ok, body}
+      {:ok, %{status: status}} -> {:error, {:http_error, status}}
+      {:error, ex} -> {:error, ex}
+    end
+  end
+
+  def get_block_txid(block_hash, index) do
+    case Req.get("https://mempool.space/api/block/#{block_hash}/txid/#{index}",
+           decode_body: false
+         ) do
+      {:ok, %{status: 200, body: body}} -> {:ok, body}
+      {:ok, %{status: status}} -> {:error, {:http_error, status}}
+      {:error, ex} -> {:error, ex}
+    end
+  end
 end
